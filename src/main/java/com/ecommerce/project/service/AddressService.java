43817046c68a6977031f4d2dcaf087ad3d4bff6b
Jessica Ferreira <fjes.ks@gmail.com>
2026-05-13 17:34:44 -0300

    Address controller and service crated. Post address endpoint created

---
+++ src/main/java/com/ecommerce/project/service/AddressService.java
@@ -0,0 +1,11 @@
+package com.ecommerce.project.service;
+
+import com.ecommerce.project.model.User;
+import com.ecommerce.project.payload.AddressDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface AddressService {
+    AddressDTO createAddress(AddressDTO addressDTO, User user);
+
+}
